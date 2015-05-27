7c2ec2c7c5e986cd3f1ba9cdf3762d3264107a48
liuchanglu <liuchanglu@gomo.com>
2015-05-27 22:50:18 +0800

    Partial implementation of displaying the numbers

--- SalesTool/src/com/gomo/salestool/SalesData.java
+++ SalesTool/src/com/gomo/salestool/SalesData.java
@@ -2,8 +2,12 @@ package com.gomo.salestool;
 
 public class SalesData {
 
+    int data[] = { 1, 4, 5, 6 };
+
     public void display() {
-        System.out.println("Sales to do");
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i]);
+        }
     }
 
 }
