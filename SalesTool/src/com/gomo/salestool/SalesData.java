af4dfe230d6ba715242ade73f2b3e9ab3ae9919c
liuchanglu <liuchanglu@gomo.com>
2015-05-27 23:00:53 +0800

    Completed displaying data in a SaleData object.

--- SalesTool/src/com/gomo/salestool/SalesData.java
+++ SalesTool/src/com/gomo/salestool/SalesData.java
@@ -5,8 +5,9 @@ public class SalesData {
     int data[] = { 1, 4, 5, 6 };
 
     public void display() {
+        System.out.println("Data: ");
         for (int i = 0; i < data.length; i++) {
-            System.out.println(data[i]);
+            System.out.println("Next value: " + data[i]);
         }
     }
 
